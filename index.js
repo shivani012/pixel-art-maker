f4312e52cee68c862e5b60fcb6532a70cf7642f6
Shruthiu2803 <shruthiu2803@gmail.com>
2018-06-11 16:27:51 +0530

    pixel art

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-document.body.innerHTML = "Hello world!";
+document.body.innerHTML = "Hello 999orld!";
+//const parentDiv = document.createElement('div')
+//parentDiv.className = "PPalette";
+for (let j=0; j<200; j++) {
+    const outDiv = document.createElement('div');
+    outDiv.className = "OPalette";
+    for (let i = 0; i < 200; i++) {
+        const div = document.createElement('div');
+        //div.innerHTML = "Color";
+        div.addEventListener('click',()=>{
+            div.style.backgroundColor = 'red';
+        })
+
+        div.className = "Palette"
+        outDiv.appendChild(div);
+    }
+    //parentDiv.appendChild(outDiv);
+    document.body.appendChild(outDiv);
+}
+const colorDiv = document.createElement('div');
+colorDiv.className = "CPalette";
+for (let i = 0; i < 2; i++) {
+    const boxDiv = document.createElement('div');
+    boxDiv.className = "boxDiv";
+    boxDiv.innerHTML = "color";
+    switch(i) {
+        case 0:
+            boxDiv.style.backgroundColor = 'blue';
+            break;
+        case 1:
+            boxDiv.style.backgroundColor = 'red';
+            break;
+    }
+
+    colorDiv.appendChild(boxDiv)
+    }
+document.body.appendChild(colorDiv);
+
