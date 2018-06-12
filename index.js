12d97953d079915f9c75d66dc16612ddc103f0f2
Shruthiu2803 <shruthiu2803@gmail.com>
2018-06-12 09:44:09 +0530

    pixel art

--- index.js
+++ index.js
@@ -1,38 +1,58 @@
-document.body.innerHTML = "Hello 999orld!";
+const mainDiv = document.createElement('div');
+mainDiv.className='Main';
+let col;
+const colorDiv = document.createElement('div');
+colorDiv.className = "CPalette";
+const availableColors = ["red","blue","yellow","green","orange","black","grey","white","brown","violet","purple","lightgrey"];
+let pick;
+for (pick of availableColors) {
+    const boxDiv = document.createElement('div');
+    boxDiv.className = "boxDiv";
+    //boxDiv.innerHTML = "color";
+    boxDiv.style.backgroundColor = pick;
+    /*for (let i = 0; i < 2; i++) {
+        const boxDiv = document.createElement('div');
+        boxDiv.className = "boxDiv";
+        boxDiv.innerHTML = "color";
+        switch(i) {
+            case 0:
+                boxDiv.style.backgroundColor = 'blue';
+                break;
+            case 1:
+                boxDiv.style.backgroundColor = 'red';
+                break;
+        }*/
+    boxDiv.addEventListener('click', () => {
+        col = boxDiv.style.backgroundColor;
+        currCol.style.backgroundColor=col;
+    })
+    colorDiv.appendChild(boxDiv)
+}
+
+const currCol = document.createElement('div');
+currCol.className='boxDiv';
+//currCol.innerHTML = "Current Color";
+
+
 //const parentDiv = document.createElement('div')
 //parentDiv.className = "PPalette";
-for (let j=0; j<200; j++) {
+for (let j=0; j<80; j++) {
     const outDiv = document.createElement('div');
     outDiv.className = "OPalette";
-    for (let i = 0; i < 200; i++) {
+    for (let i = 0; i < 80; i++) {
         const div = document.createElement('div');
         //div.innerHTML = "Color";
         div.addEventListener('click',()=>{
-            div.style.backgroundColor = 'red';
+            div.style.backgroundColor =col;
         })
 
         div.className = "Palette"
         outDiv.appendChild(div);
     }
     //parentDiv.appendChild(outDiv);
-    document.body.appendChild(outDiv);
+    mainDiv.appendChild(outDiv);
 }
-const colorDiv = document.createElement('div');
-colorDiv.className = "CPalette";
-for (let i = 0; i < 2; i++) {
-    const boxDiv = document.createElement('div');
-    boxDiv.className = "boxDiv";
-    boxDiv.innerHTML = "color";
-    switch(i) {
-        case 0:
-            boxDiv.style.backgroundColor = 'blue';
-            break;
-        case 1:
-            boxDiv.style.backgroundColor = 'red';
-            break;
-    }
-
-    colorDiv.appendChild(boxDiv)
-    }
-document.body.appendChild(colorDiv);
+mainDiv.appendChild(colorDiv);
+mainDiv.appendChild(currCol);
+document.body.appendChild(mainDiv);
 
